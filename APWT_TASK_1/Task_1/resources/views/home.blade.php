b4d83f1ac7777722f196d6100638fe61e424d90f
Emrul-Hasan <imrulhasan003@gmail.com>
2022-06-25 03:32:27 +0600

    Lab 1 ScreenShot Add

--- APWT_TASK_1/Task_1/resources/views/home.blade.php
+++ APWT_TASK_1/Task_1/resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<h2 style="text-align: center; color:blue">Our Home Page</h2>
 <div style="padding:20px;">
 <img style="width: 100%; height:50% " src="https://www.linkpicture.com/q/images2.jpg" alt="">
 <h2 style="text-align: center; margin-top:10px">Isometric Pharma</h2>
